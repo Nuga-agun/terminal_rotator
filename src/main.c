c9b5a6927465152e5c6f6ca39a40eb286e97b6c9
Nuga <nuga_agun@proton.me>
2025-10-08 13:51:09 +0200

    Construction de la grille

--- src/main.c
+++ src/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-int main() {
-	printf("Hello, world!");
+#define GRID_SIZE 20
+#define EMPTY_CHAR ' '
+
+void draw_grid(char grid[GRID_SIZE][GRID_SIZE]) {
+	for (int i=0; i<GRID_SIZE; i++) {
+		for (int j=0; j<GRID_SIZE; j++) {
+			printf("%c", grid[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void init_grid(char grid[GRID_SIZE][GRID_SIZE]) {
+	for (int i=0; i<GRID_SIZE; i++) {
+		for (int j=0; j<GRID_SIZE; j++) {
+			grid[i][j] = EMPTY_CHAR;
+		}
+	}
+}
+
+int main(int argc, char *argv[]) {
+	char grid[GRID_SIZE][GRID_SIZE];
+
+	init_grid(grid);
+	draw_grid(grid);
+
 	return 0;
 }
