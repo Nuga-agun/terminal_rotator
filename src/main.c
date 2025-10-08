bcee2d02022d2dc319511bbb96c529521fa44e3d
Nuga <nuga_agun@proton.me>
2025-10-08 14:33:19 +0200

    Affichage d'un message entré par l'utilisateur

--- src/main.c
+++ src/main.c
@@ -2,6 +2,11 @@
 
 #define GRID_SIZE 20
 #define EMPTY_CHAR ' '
+#define MAX_LENGTH_INPUT 10
+
+void clear_screen() {
+	printf("\033[2J\033[1;1H");
+}
 
 void draw_grid(char grid[GRID_SIZE][GRID_SIZE]) {
 	for (int i=0; i<GRID_SIZE; i++) {
@@ -22,8 +27,18 @@ void init_grid(char grid[GRID_SIZE][GRID_SIZE]) {
 
 int main(int argc, char *argv[]) {
 	char grid[GRID_SIZE][GRID_SIZE];
+	char user_input[MAX_LENGTH_INPUT];
+	char buffer[MAX_LENGTH_INPUT];
 
 	init_grid(grid);
+	printf("Entrez le texte à animer : ");
+	scanf("%[^\n]", user_input);
+	int middle_index = GRID_SIZE/2;
+	for (int j=0; j<MAX_LENGTH_INPUT; j++) {
+		grid[middle_index][j + middle_index] = user_input[j];
+	}
+
+	clear_screen();
 	draw_grid(grid);
 
 	return 0;
