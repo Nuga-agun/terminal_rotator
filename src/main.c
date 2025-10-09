04b8ee902139e0de2d5657a8ae447d3069800518
Nuga <nuga_agun@proton.me>
2025-10-09 07:45:57 +0200

    Rotation

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <math.h>
 
 #define GRID_SIZE 20
 #define EMPTY_CHAR ' '
-#define MAX_LENGTH_INPUT 10
+#define LEN_INPUT 10
+#define REFRESH_DELAY 1000000
+
+struct point{
+	float x;
+	float y;
+	char letter;
+};
 
 void clear_screen() {
 	printf("\033[2J\033[1;1H");
@@ -25,21 +34,47 @@ void init_grid(char grid[GRID_SIZE][GRID_SIZE]) {
 	}
 }
 
+int write_in_grid(struct point p, char grid[GRID_SIZE][GRID_SIZE]) {
+	int i = (int) p.y + GRID_SIZE/2;
+	int j = (int) p.x + GRID_SIZE/2;
+	if (i>=GRID_SIZE || j>=GRID_SIZE) {
+		return 1;
+	}
+	grid[i][j] = p.letter;
+	return 0;
+}
+
 int main(int argc, char *argv[]) {
 	char grid[GRID_SIZE][GRID_SIZE];
-	char user_input[MAX_LENGTH_INPUT];
-	char buffer[MAX_LENGTH_INPUT];
+	char user_input[LEN_INPUT];
+	char buffer[LEN_INPUT];
 
-	init_grid(grid);
 	printf("Entrez le texte à animer : ");
 	scanf("%[^\n]", user_input);
-	int middle_index = GRID_SIZE/2;
-	for (int j=0; j<MAX_LENGTH_INPUT; j++) {
-		grid[middle_index][j + middle_index] = user_input[j];
+	struct point message_points[LEN_INPUT];
+	for (int index=0; index<LEN_INPUT; index++) {
+		message_points[index].x = index;
+		message_points[index].y = 0;
+		message_points[index].letter = user_input[index];
 	}
 
-	clear_screen();
-	draw_grid(grid);
+	while (1) {
+		clear_screen();
+		init_grid(grid);
+
+		for (int index=0; index<LEN_INPUT; index++) {
+			float old_x = message_points[index].x;
+			float old_y = message_points[index].y;
+			message_points[index].x = old_x * cos(M_PI/6) - old_y * sin(M_PI/6);
+			message_points[index].y = old_x * sin(M_PI/6) + old_y * cos(M_PI/6);
+			if (write_in_grid(message_points[index], grid) != 0) {
+				return 1;
+			}
+		}
+
+		draw_grid(grid);
+		usleep(REFRESH_DELAY);
+	}
 
 	return 0;
 }
